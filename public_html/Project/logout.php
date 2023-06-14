750865812f518c0418ffec6078453b79f0f98982
DanielUrbina <danileur1999@gmail.com>
2023-06-14 16:09:59 -0400

    login part 2

---
+++ public_html/Project/logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start(); // start the session
+session_unset(); // clear out all the variables
+session_destroy(); // clean it up from the server
+/* 
+redirect the user back to login, no die or exit here because 
+it's the end of the script there's nothing else to terminate here, 
+we usually use a die or exit when there's still other code after this point 
+*/
+header("Location: login.php");
