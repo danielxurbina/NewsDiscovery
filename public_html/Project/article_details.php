aaae1b0c5f611968c1fbf09481f90893c8411c28
DanielUrbina <danileur1999@gmail.com>
2023-07-23 19:33:03 -0400

    fixed some styling components for the app

--- public_html/Project/article_details.php
+++ public_html/Project/article_details.php
@@ -1,5 +1,12 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
+
+// If user is not logged in redirect to login page
+if(!is_logged_in()){
+    flash("You must be logged in to view this page", "warning");
+    die(header("Location: login.php"));
+}
+
 ?>
 
 <div class="container-fluid">
@@ -25,19 +32,29 @@ require(__DIR__ . "/../../partials/nav.php");
             redirect("home.php");
         }
     ?>
-    <img src="<?php echo $result['image_url'];?>" alt="Article Image">
-    <h1><?php echo $result['title'];?></h1>
-    <h3><?php echo $result['publish_date'];?></h3>
-    <h3>Source: <?php echo $result['source_id'];?></h3>
-    <h3><?php echo $result['category'];?></h3>
-    <h3><?php echo $result['country'];?></h3>
-    <a class="btn btn-primary" href="<?php echo $result['link'];?>">Link to article</a>
-    <?php if($result['created_by'] == get_user_id() || has_role("Admin")) : ?>
-        <a class="btn btn-warning" href="article_edit.php?id=<?php echo $result['id'];?>">Edit</a>
-        <a class="btn btn-danger" href="article_delete.php?id=<?php echo $result['id'];?>">Delete</a>
-    <?php endif; ?>
-    <p><?php echo $result['content_description'];?></p>
-    <p><?php echo $result['content'];?></p>
+    <div class="container">
+        <div class="item display-card">
+            <img id="articleImage" src="<?php echo $result['image_url'];?>" alt="Article Image">
+            <h1 id="articleTitle"><?php echo $result['title'];?></h1>
+            <h3 id="publishDate"><?php echo $result['publish_date'];?></h3>
+            <h3 id="articleSource">Source: <?php echo $result['source_id'];?></h3>
+            <h3 id="articleCategory"><?php echo $result['category'];?></h3>
+            <h3 id="articleCountry"><?php echo $result['country'];?></h3>
+            <a id="articleLink" class="btn btn-primary" href="<?php echo $result['link'];?>">Link to article</a>
+            <?php if($result['created_by'] == get_user_id() || has_role("Admin")) : ?>
+                <a id="articleEdit" class="btn btn-warning" href="article_edit.php?id=<?php echo $result['id'];?>">Edit</a>
+                <a id="articleDelete" class="btn btn-danger" href="article_delete.php?id=<?php echo $result['id'];?>">Delete</a>
+            <?php endif; ?>
+            <p id="articleDescription"><?php echo $result['content_description'];?></p>
+            <p id="articleContent"><?php echo $result['content'];?></p>
+        </div>
+    </div>
 </div>
+
+<style>
+    .container{
+        display: grid;
+    }
+</style>
 
 <?php require(__DIR__ . "/../../partials/flash.php"); ?>
--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,16 +1,22 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
+
+// If user is not logged in redirect to login page
+if(!is_logged_in()){
+    flash("You must be logged in to view this page", "warning");
+    die(header("Location: login.php"));
+}
+
 ?>
 
 <div class="container-fluid">
-    <div class="row">
-        <h1> News </h1>
-        <p> Welcome to the News page! </p>
-        <p> Here you can view all the news articles that have been created. </p>
-        <p> Type something in the input field to search the list of articles for specific items: </p>
-    </div>
-    <div class="row">
-        <div class="col">
+        <div class="homepage-info">
+            <h1> News </h1>
+            <p> Welcome to the News page! </p>
+            <p> Here you can view all the news articles that have been created. </p>
+            <p> Type something in the input field to search the list of articles for specific items: </p>
+        </div>
+        <div class="filter-section">
             <form method="POST" id="filterForm" action="">
                 <div class="col">
                     <div class="form-check form-switch">
@@ -24,7 +30,7 @@ require(__DIR__ . "/../../partials/nav.php");
                 </div>
             </form>
         </div>
-        <div class="row">
+        <div class="search-section">
             <form method="POST" id="searchForm" action="">
                 <div class="col">
                     <input class="form-control" id="searchInput" name="searchInput" type="text" placeholder="Search..">
@@ -34,134 +40,81 @@ require(__DIR__ . "/../../partials/nav.php");
                 </div>
             </form>
         </div>
-    </div>
-    <div class="row" id="articles">
-        <?php
-        // Set the default number of articles to display
-        $article_limit = 10;
-        $articles = [];
-        $categories = [];
-        $countries = [];
+        <?php         
+            // Set the default number of articles to display
+            $article_limit = 10;
+            $articles = [];
+            $categories = [];
+            $countries = [];
 
-        // Check if the user has entered a number of articles to display
-        if (isset($_POST['articleLimit'])) {
-            $article_limit = $_POST['articleLimit'];
-            $articles = get_articles($article_limit);
-            error_log("Article Limit: " . var_export($articles, true));
-        }
-        else if(isset($_POST['searchInput'])){
-            $searchInput = $_POST['searchInput'];
-            $articles = searchFilter('NewsArticles', $searchInput);
-            // If the articles array is empty display a message that no articles were found
-            if(empty($articles)){
-                flash("No articles found", "info");
+            // Check if the user has entered a number of articles to display
+            if (isset($_POST['articleLimit'])) {
+                $article_limit = $_POST['articleLimit'];
+                $articles = get_articles($article_limit);
+                error_log("Article Limit: " . var_export($articles, true));
             }
-            error_log("Search Input: " . var_export($articles, true));
-        }
-        else{
-            $articles = get_articles($article_limit);
-            error_log("Articles: " . var_export($articles, true));
-        }
+            else if(isset($_POST['searchInput'])){
+                $searchInput = $_POST['searchInput'];
+                $articles = searchFilter('NewsArticles', $searchInput);
+                // If the articles array is empty display a message that no articles were found
+                if(empty($articles)){
+                    flash("No articles found", "info");
+                }
+                error_log("Search Input: " . var_export($articles, true));
+            }
+            else{
+                $articles = get_articles($article_limit);
+                error_log("Articles: " . var_export($articles, true));
+            }
 
-        // Fetch all categories from the database
-        $categories = get_categories();
+            // Fetch all categories from the database
+            $categories = get_categories();
 
-        // Fetch all countries from the database
-        $countries = get_countries();
+            // Fetch all countries from the database
+            $countries = get_countries();
 
-        error_log("Categories: " . var_export($categories, true));
-        error_log("Countries: " . var_export($countries, true));
+            error_log("Categories: " . var_export($categories, true));
+            error_log("Countries: " . var_export($countries, true));
 
-        // Loop through the articles and display each one
-        foreach ($articles as $article) {
-            error_log("Article: " . var_export($article, true));
-            $queryParam = "article_" . $article['id'];
-            ?>
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <?php
-                    // Check if the article has a valid image_url
-                    if (!empty($article['image_url'])) {
-                        ?>
-                        <img src="<?php echo $article['image_url']; ?>" class="card-img-top" alt="<?php echo $article['title']; ?>">
+            // Loop through the articles and display each one
+            foreach ($articles as $article) {
+                error_log("Article: " . var_export($article, true));
+                $queryParam = "article_" . $article['id'];
+        ?>
+                <div class="container mt-3">
+                    <div class="card">
                         <?php
-                    } else {
-                        // If image_url is null, display a random image from the specified URL
+                            // Check if the article has a valid image_url
+                            if (!empty($article['image_url'])) {
+                                ?>
+                                <img src="<?php echo $article['image_url']; ?>" class="card-img-top" alt="<?php echo $article['title']; ?>" style="width:100%">
+                                <?php
+                            } else {
+                                // If image_url is null, display a random image from the specified URL
+                                ?>
+                                <img src="https://source.unsplash.com/user/c_v_r/?<?php echo $queryParam; ?>" class="card-img-top" alt="<?php echo $article['title']; ?>">
+                                <?php
+                            }
                         ?>
-                        <img src="https://source.unsplash.com/user/c_v_r/?<?php echo $queryParam; ?>" class="card-img-top" alt="<?php echo $article['title']; ?>">
-                        <?php
-                    }
-                    ?>
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $article['title']; ?></h5>
-                        <p class="card-text"><?php echo $article['content_description']; ?></p>
-                        <a href="article_details.php?id=<?php echo $article['id']; ?>" class="btn btn-primary">Read More</a>
-                        <?php if ($article['created_by'] == get_user_id()) : ?>
-                            <a href="article_edit.php?id=<?php echo $article['id']; ?>" class="btn btn-warning">Edit</a>
-                            <a href="article_delete.php?id=<?php echo $article['id']; ?>" class="btn btn-danger">Delete</a>
-                        <?php endif; ?>
-                        <?php if(has_role("Admin") && $article['created_by'] != get_user_id()) : ?>
-                            <a href="article_edit.php?id=<?php echo $article['id']; ?>" class="btn btn-warning">Edit</a>
-                            <a href="article_delete.php?id=<?php echo $article['id']; ?>" class="btn btn-danger">Delete</a>
-                        <?php endif ?>
+                        <div class="card-body">
+                            <h4 class="card-title"><?php echo $article['title']; ?></h4>
+                            <p class="card-text"><?php echo $article['content_description']; ?></p>
+                            <a href="article_details.php?id=<?php echo $article['id']; ?>" class="btn btn-primary">Read More</a>
+                            <?php if ($article['created_by'] == get_user_id()) : ?>
+                                <a href="article_edit.php?id=<?php echo $article['id']; ?>" class="btn btn-warning">Edit</a>
+                                <a href="article_delete.php?id=<?php echo $article['id']; ?>" class="btn btn-danger">Delete</a>
+                            <?php endif; ?>
+                            <?php if(has_role("Admin") && $article['created_by'] != get_user_id()) : ?>
+                                <a href="article_edit.php?id=<?php echo $article['id']; ?>" class="btn btn-warning">Edit</a>
+                                <a href="article_delete.php?id=<?php echo $article['id']; ?>" class="btn btn-danger">Delete</a>
+                            <?php endif ?>
+                        </div>
                     </div>
                 </div>
-            </div>
         <?php
-        }
+            }
         ?>
-    </div>
 </div>
-
-
-<style>
-    .container-fluid {
-        padding: 20px;
-    }
-
-    .card {
-        width: 100%;
-        border: 1px solid #e6e6e6;
-        border-radius: 4px;
-        transition: box-shadow 0.3s ease;
-    }
-
-    .card:hover {
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    }
-
-    .card-img-top {
-        height: 200px;
-        object-fit: cover;
-        border-top-left-radius: 4px;
-        border-top-right-radius: 4px;
-    }
-
-    .card-body {
-        padding: 10px;
-    }
-
-    .card-title {
-        font-size: 18px;
-        margin-bottom: 10px;
-    }
-
-    .card-text {
-        font-size: 14px;
-        color: #666;
-    }
-
-    .btn {
-        margin-top: 10px;
-    }
-
-    @media (max-width: 767px) {
-        .col-md-4 {
-            flex-basis: 100%;
-            max-width: 100%;
-        }
-    }
-</style>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
